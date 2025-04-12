4eb517fc999a8a6616f111c8fe6cb306edcf2c1f
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-12 11:38:51 -0400

    ARCHIVOS INICIALES

---
+++ src/main/java/com/sumativa2/exp2_s5_briam_carrasco/Exp2S5BriamCarrascoApplication.java
@@ -0,0 +1,13 @@
+package com.sumativa2.exp2_s5_briam_carrasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp2S5BriamCarrascoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp2S5BriamCarrascoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumativa2/exp2_s5_briam_carrasco/Exp2S5BriamCarrascoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumativa2.exp2_s5_briam_carrasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp2S5BriamCarrascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
