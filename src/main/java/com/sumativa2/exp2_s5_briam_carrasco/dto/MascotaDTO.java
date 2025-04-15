5e2a4020de67327687a6634e0a10aaee2a39444c
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-14 21:45:37 -0400

    SE CREA DTO PARA MASCOTA

---
+++ src/main/java/com/sumativa2/exp2_s5_briam_carrasco/dto/MascotaDTO.java
@@ -0,0 +1,16 @@
+package com.sumativa2.exp2_s5_briam_carrasco.dto;
+
+import lombok.Data;
+
+@Data
+public class MascotaDTO {
+    private String nombreMascota;
+    private String edadMascota;
+    private String especieMascota;
+    private String razaMascota;
+    private String colorMascota;
+    private String generoMascota;
+    private Long participanteId;
+}
+
+
